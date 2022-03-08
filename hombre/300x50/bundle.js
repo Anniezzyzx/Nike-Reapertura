0d563ad445fb5681646a561bbfd44d638c6c305c
Anniezzyzx <46903927+Anniezzyzx@users.noreply.github.com>
2022-03-08 16:27:56 -0300

    hombre

---
+++ hombre/300x50/bundle.js
@@ -0,0 +1,145 @@
+(function () {
+  'use strict';
+
+  // BannerUtils version 3.2.0
+  function getBrowser() {
+    // desktop browsers as of 2019-10-04
+    var browserslist = ['other', 'blink', 'chrome', 'safari', 'opera', 'ie', 'edge', 'firefox'];
+    var browser = 0;
+
+    if ('WebkitAppearance' in document.documentElement.style) {
+      browser = 1; // chrome/safari/opera/edge/firefox
+
+      if (/google/i.test(window.navigator.vendor)) browser = 2;
+      if (/apple/i.test(window.navigator.vendor)) browser = 3;
+      if (!!window.opr && !!window.opr.addons || !!window.opera || / OPR\//.test(window.navigator.userAgent)) browser = 4;
+    }
+
+    if (
+    /*@cc_on!@*/
+     !!document.documentMode) browser = 5; // ie 6-11
+
+    if (browser !== 5 && !!window.StyleMedia) browser = 6;
+    if (typeof InstallTrigger !== 'undefined' || 'MozAppearance' in document.documentElement.style) browser = 7;
+    return browserslist[browser];
+  }
+  var browser = getBrowser();
+  function es5() {
+    return parseInt('010', 10) === 10 && function () {
+      return !this;
+    }() && !!(Date && Date.prototype && Date.prototype.toISOString); // IE10, FF21, CH23, SF6, OP15, iOS7, AN4.4
+  }
+  var log = {
+    // https://bit.ly/32ZIpgo
+    traceOn: window.console.log.bind(window.console, '%s'),
+    traceOff: function traceOff() {},
+    trace: window.console.log.bind(window.console, '%s'),
+
+    set debug(bool) {
+      this._debug = bool;
+      bool ? this.trace = this.traceOn : this.trace = this.traceOff;
+    },
+
+    get debug() {
+      return this._debug;
+    }
+
+  };
+  function domIds(scope) {
+    if (scope === void 0) {
+      scope = document;
+    }
+
+    var all = scope.getElementsByTagName('*');
+    var haveIds = {};
+    var i = all.length;
+
+    while (i--) {
+      if (all[i].id) {
+        var safeId = all[i].id.replace(/-|:|\./g, '_');
+        haveIds[safeId] = all[i];
+      }
+    }
+
+    return haveIds;
+  }
+
+  var Banner = {
+    init: function init() {
+      log.debug = true; // set to false before publishing
+
+      var dom = domIds(); ////////////////////////////////////////////////////// ANIMATION //////////////////////////////////////////////////////
+
+      function frameStart() {
+        if (es5()) {
+          frame0();
+        } else {
+          dom.backup.classList.add('backup');
+        }
+      }
+
+      function frame0() {
+        var tl = gsap.timeline({
+          defaults: {},
+          onComplete: addRollover
+        });
+        tl.to('#img_1', {
+          duration: 0.5,
+          x: 114,
+          ease: Cubic.easeInOut
+        }, 1.5).from('#txt_1', {
+          duration: 0.5,
+          scale: 1.4,
+          autoAlpha: 0
+        }, 0.2).to('#img_1', {
+          duration: 0.5,
+          x: 228,
+          ease: Cubic.easeInOut
+        }, '+=1.7').from('#txt_2', {
+          duration: 0.5,
+          scale: 1.4,
+          autoAlpha: 0
+        }, 1.2).from('#logo', {
+          duration: 0.5,
+          scale: 1.4,
+          autoAlpha: 0
+        }, 2.3).from('#txt_3,#cta', {
+          duration: 0.5,
+          autoAlpha: 0
+        }, 2.5);
+        dom.ad_content.classList.remove('invisible');
+      } ////////////////////////////////////////////////////// EVENT HANDLERS //////////////////////////////////////////////////////
+
+
+      function addRollover() {
+        dom.ad_content.addEventListener('mouseenter', function () {
+          gsap.to('#cta', 0.1, {
+            scale: 1.05,
+            force3D: false
+          }); // Hover enter code goes here. Please remove this comment.
+        });
+        dom.ad_content.addEventListener('mouseleave', function () {
+          gsap.to('#cta', 0.1, {
+            scale: 1,
+            force3D: false
+          }); // Hover out code goes here. Please remove this comment.
+        });
+      }
+
+      function adClickThru() {
+        dom.ad_content.addEventListener('click', function () {
+          window.open(window.clickTag || window.clickTAG);
+        });
+      } ////////////////////////////////////////////////////// INIT //////////////////////////////////////////////////////
+
+
+      adClickThru();
+      frameStart();
+    }
+  };
+
+  window.onload = function () {
+    window.requestAnimationFrame(Banner.init);
+  };
+
+}());
